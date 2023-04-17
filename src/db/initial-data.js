484bbf2ddd2690090b938a7458f9abac8e6420e6
Joe Alt <joejoyalt@gmail.com>
2023-04-17 17:39:07 +0200

    typo change

--- src/db/initial-data.js
+++ src/db/initial-data.js
@@ -1,7 +1,7 @@
 const initialData = {
   tasks: {
     'task-1': { id: 'task-1', content: 'Take out the garbage' },
-    'task-2': { id: 'task-2', content: 'Relocate my desktop' },
+    'task-2': { id: 'task-2', content: 'Relocate my desktop to living room' },
     'task-3': { id: 'task-3', content: 'Charge my phone' },
     'task-4': { id: 'task-4', content: 'Cook dinner' },
   },
